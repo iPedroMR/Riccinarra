2679a2da78f31f4f80540be13e180c9e5523cf27
Lucas Rezende <lucas-mr@hotmail.com.br>
2021-08-16 22:57:14 -0300

    Texto

--- js/main.js
+++ js/main.js
@@ -185,4 +185,31 @@ jQuery(document).ready(function ($) {
       },
     },
   });
+
+  $("#customers-blog").owlCarousel({
+    loop: true,
+    center: true,
+    items: 1,
+    margin: 30,
+    autoplay: true,
+    dots: true,
+    nav: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1170: {
+        items: 3,
+      },
+    },
+  });
 });
